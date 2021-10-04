c771727e0bff6f458acf744ba0bf124a90aba13c
ns-maud <nastya.molchanova96@gmail.com>
2021-10-04 03:15:43 +0300

    Homework 17-18 is done.

---
+++ app/src/main/java/com/example/myapplication/homework17_18/Discipline.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.homework17_18
+
+data class Discipline(
+    val disciplineName: String,
+    val disciplineId: Int
+)
---
+++ app/src/main/java/com/example/myapplication/homework17_18/Student.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.homework17_18
+
+data class Student(
+    val studentName: String,
+    val studentId: Int
+)
